778334e0882194c7e52d932c6a1551d6d18d8c6e
sathishoktase <sathishoktase@gmail.com>
2020-12-14 16:52:25 +1100

    Fixed minor issues with token introspection expiry format and userinfo end-point preferred_username claim

--- src/apiproxies/oidc/apiproxy/resources/jsc/ReplaceUserInfoSubWithPPID.js
+++ src/apiproxies/oidc/apiproxy/resources/jsc/ReplaceUserInfoSubWithPPID.js
@@ -1,3 +1,4 @@
 var payload = JSON.parse(context.getVariable("response.content"));
 payload.sub = context.getVariable("oauthv2accesstoken.OAInfo-RetrieveAccessTokenDetails.accesstoken.customerPPId");
+payload.preferred_username = payload.given_name;
 context.setVariable("response.content", JSON.stringify(payload));
--- src/apiproxies/oidc/apiproxy/resources/jsc/SetIntrospectResponse.js
+++ src/apiproxies/oidc/apiproxy/resources/jsc/SetIntrospectResponse.js
@@ -2,10 +2,8 @@ var refreshTokenStatus = context.getVariable("oauthv2refreshtoken.OAInfo-Retriev
 
 if (refreshTokenStatus === "approved") {
     context.setVariable("isActive", true);
-    var refreshTokenExpiresIn = context.getVariable("oauthv2refreshtoken.OAInfo-RetrieveRefreshTokenDetails.refresh_token_expires_in");
-    var date = Date.now();
-    var refreshTokenExpiresAt = Math.trunc((date + (refreshTokenExpiresIn * 1000))/1000);
-    context.setVariable("tokenExp",refreshTokenExpiresAt);
+    var refreshTokenExpiresAt = Math.trunc(Date.now()/1000) + Number(context.getVariable("oauthv2refreshtoken.OAInfo-RetrieveRefreshTokenDetails.refresh_token_expires_in"));
+    context.setVariable("tokenExp",refreshTokenExpiresAt.toString());
 } else {
     context.setVariable("isActive", false);
     context.setVariable("tokenExp",null);
