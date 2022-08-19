be7a204c5df0a852e0c50e7ed927341f552d3745
Debora Elkin <deboraelkin@google.com>
2022-08-19 14:11:31 +1000

    Added copyright banner

--- setup/deployOidcMockProviderGAE.sh
+++ setup/deployOidcMockProviderGAE.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
-
+#
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # deploy oidc-mock-provider app as a Google App Engine app
-
-
-# current directory assumption: <repo-root-folder>
-
-# dependencies:
-# utils: gcloud
-# 
-# roles:
-# roles/appengine.appCreator # create app
-# roles/appengine.appAdmin # admin app
-# roles/storage.admin
-# roles/cloudbuild.builds.editor
 
 if [ "$#" -ne 1 ]; then
     echo "This script deploys a mock OIDC Identity provider for the CDS reference implementation as a Google App Engine service"
